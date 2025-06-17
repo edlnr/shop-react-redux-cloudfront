450955bb1eebd20746225cd2365197103e6932e5
Eduard Lainer <eduard.lainer_ext@novartis.com>
2025-06-17 12:06:51 +0300

    Task 8: Updated cart endpoint

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://ptsih27azj.execute-api.us-east-1.amazonaws.com/prod",
   bff: "https://k6wdehcw8d.execute-api.us-east-1.amazonaws.com/prod",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "https://ughcnfs0vj.execute-api.us-east-1.amazonaws.com/prod/",
 };
 
 export default API_PATHS;
